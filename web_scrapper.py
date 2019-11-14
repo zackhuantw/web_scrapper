e1f86dcac2e7328feb7db055f6ded16670c6fe76
Zack Huan <zack.huan@thoughtworks.com>
2019-11-14 00:40:26 -0500

    Add comment

--- web_scrapper.py
+++ web_scrapper.py
@@ -9,6 +9,8 @@ driver.implicitly_wait(1000)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 driver.quit()
 
+# TODO: Consider pricing logic to decide whether to show address
+
 for listing in soup.find_all('div', class_='card-wrapper card-wrapper--property grid-cell'):
     address = listing.find('h2', class_='property-card-content__address').text
     price = listing.find('span', class_='details__price-val').text
